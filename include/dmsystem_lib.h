0b9078ecde859b9b044673ddb86eb9d5af53c1c9
Patryk Kubiak <patryk.kubiak90@gmail.com>
2026-07-18 11:54:18 +0200

    Added empty lib files

---
+++ include/dmsystem_lib.h
@@ -0,0 +1,7 @@
+#ifndef DMSYSTEM_LIB_H
+#define DMSYSTEM_LIB_H
+
+#include "dmod.h"
+#include "dmsystem_lib_defs.h"
+
+#endif // DMSYSTEM_LIB_H
--- src/dmsystem.c
+++ src/dmsystem.c
@@ -1,26 +1,6 @@
 #include "dmod.h"
 
 /**
- * @brief Pre-initialization function for the module.
- * 
- * @note This function is optional. You can remove it if you don't need it.
- * 
- * This function is called when the module enabling is in progress.
- * 
- * You can use this function to load the required dependencies, such as 
- * other modules. Please be aware that the module is not fully initialized, 
- * so not all the API functions are available - you can check if the API
- * is connected by calling the Dmod_IsFunctionConnected() function.
- */
-void dmod_preinit(void)
-{
-    if(Dmod_IsFunctionConnected( Dmod_Printf ))
-    {
-        Dmod_Printf("API is connected!\n");
-    }
-}
-
-/**
  * @brief Main function of the application
  * 
  * @param argc Number of arguments
